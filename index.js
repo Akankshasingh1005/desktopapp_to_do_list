be739a7c5611c1fea202513418af1ebb092aa0fe
Akanksha Singh <b23cm1005@iitj.ac.in>
2025-07-17 16:25:13 +0530

    just a cutesy to do list commit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const path = require("path");
 const { app, BrowserWindow, ipcMain } = require("electron");
-const fs = require("fs");
+const path = require("path");
 
 let win;
 
@@ -9,15 +8,15 @@ function createWindow() {
     width: 550,
     height: 600,
     webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true,
       preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
     },
+    resizable: false,
   });
 
   win.removeMenu();
   win.loadFile("index.html");
-  
+
   ipcMain.on("load-page", (event, page) => {
     win.loadFile(page);
   });
@@ -25,9 +24,4 @@ function createWindow() {
 
 app.whenReady().then(() => {
   createWindow();
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
 });
